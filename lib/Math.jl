e081227d96ab2996211477a4ea6f98b57c50feb1
amenoyoya <mtyswe3@gmail.com>
2019-11-17 21:46:27 +0900

    add: 誤差逆伝播法による二層ニューラルネットワーク

--- lib/Math.jl
+++ lib/Math.jl
@@ -30,6 +30,25 @@ numeric_gradient(f, x::Array{Float64,1})::Array{Float64,1} = begin
     return grad
 end
 
+# 数値微分による勾配計算（バッチ対応版）
+## 関数(Array{Float64,2})::Float64, 入力値::Array{Float64,2} -> 勾配::Array{Float64,2}
+numeric_gradient(f, x::Array{Float64,2})::Array{Float64,2} = begin
+    h = 1e-4 # 10^(-4)
+    grad = Array{Float64, 2}(undef, size(x, 1), size(x, 2)) # xと同じ次元の行列を生成
+    # 各変数ごとの数値微分を行列にまとめる
+    for row in 1:size(x, 1), col in 1:size(x, 2)
+        # 指定indexの変数に対する中心差分を求める
+        org = x[row, col]
+        x[row, col] = org + h
+        f1 = f(x) # f([..., x[row, col] + h, ...]) -> Float64
+        x[row, col] = org - h
+        f2 = f(x) # f([..., x[row, col] - h, ...]) -> Float64
+        grad[row, col] = (f1 - f2) / 2h # (row, col)番目の変数に対する数値微分
+        x[row, col] = org # x[i]の値をもとに戻す
+    end
+    return grad
+end
+
 # 勾配降下法
 ## 損失関数 f, ベクトル x, 学習率 lr, 試行回数 ste_num -> 最適化ベクトル
 gradient_descent(f, x::Array{Float64,1}, lr::Float64=0.01, step_num::Int=100)::Array{Float64,1} = begin
